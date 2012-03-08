c701c0e59ccac599f8a402f8ae18b5852bdd4667
Devon <devon@Jupiter.(none)>
2012-03-08 00:08:21 -0500

    wrote spec for shortener get to return json

--- spec/shorten_spec.rb
+++ spec/shorten_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'rspec'
 require 'rack/test'
+require 'json'
 
 set :environment, :test
 
@@ -15,5 +16,11 @@ describe "basic functionality" do
     get '/'
     last_response.should be_ok
     last_response.body.should == "hello world"
+  end
+
+  it "returns a json object containing a url" do
+	  get '/shorten?url=www.google.com'
+	  parsed_body = JSON.parse last_response.body
+	  parsed_body["url"].nil?.should == false
   end
 end
