fa0221fa1bf09988a9ac8577360a3703ac20ca34
dpeticol <devon.peticolas@gmail.com>
2012-03-08 00:32:34 -0500

    finished specs for getters

--- spec/shorten_spec.rb
+++ spec/shorten_spec.rb
@@ -6,35 +6,47 @@ require 'json'
 set :environment, :test
 
 describe "basic functionality" do
-  include Rack::Test::Methods
-  
-  def app
-    Sinatra::Application
-  end
-  
-  it "say 'hello world'" do
-    get '/'
-    last_response.should be_ok
-    last_response.body.should == "hello world"
-  end
+	include Rack::Test::Methods
+	
+	def app
+		Sinatra::Application
+	end
+	
+	it "say 'hello world'" do
+		get '/'
+		last_response.should be_ok
+		last_response.body.should == "hello world"
+	end
 
-  it "returns a json object containing a url" do
-	  get '/shorten?url=www.google.com'
-	  parsed_body = JSON.parse last_response.body
-	  parsed_body["url"].nil?.should == false
-  end
 end
 
 describe "URL shortener" do
-  it "should redirect to home if no url given" do
-    get '/shorten?'
-    last_response.status.should == 301
-    last_response.headers["location"].should == "http://localhost:4567/"
-  end
-  
-  it "should redirect to home if no such shortened url exists" do
-    get '/NOTEXISTANT'
-    last_response.status.should == 301
-    last_response.headers["location"].should == "http://localhost:4567/"
-  end
+	
+	it "should return a json object containing a url" do
+		get '/shorten?url=www.google.com'
+		parsed_body = JSON.parse last_response.body
+		parsed_body["url"].nil?.should == false
+	end
+ 
+	it "should redirect to home if no url given" do
+		get '/shorten?'
+		last_response.status.should == 301
+		last_response.headers["location"].should == "http://localhost:4567/"
+	end
+end
+
+# TODO : come up with better URLs to be testing on
+describe "URL redirector" do
+
+	it "should redirect to home if no such shortened url exists" do
+		get '/NOTEXISTANT'
+		last_response.status.should == 301
+		last_response.headers["location"].should == "http://localhost:4567/"
+	end
+
+	it "should redirect me to google.com" do
+		get '/EXISTANT'
+		last.response.status.should == 200
+		last_respnse.headers["location"].should == "http://www.google.com" # using google bad?
+	end
 end
