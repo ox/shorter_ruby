4bdc33c4c1b532b63871615de8d6e7429ebfdd1d
Artem Titoulenko <artem.titoulenko@gmail.com>
2012-03-07 23:32:43 -0500

    initial hello world and first spec

---
+++ spec/shorten_spec.rb
@@ -0,0 +1,19 @@
+require "server.rb"
+require 'rspec'
+require 'rack/test'
+
+set :environment, :test
+
+describe "basic functionality" do
+  include Rack::Test::Methods
+  
+  def app
+    Sinatra::Application
+  end
+  
+  it "says 'hello world'" do
+    get '/'
+    last_response.should be_ok
+    last_response.body.should == "hello world"
+  end
+end
