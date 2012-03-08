07fcd3a59c953174b4c4d7512c8ed8fc2b3a7db3
Artem Titoulenko <artem.titoulenko@gmail.com>
2012-03-08 11:43:36 -0500

    /shorten redirects on empty url, some standard sinatra settings added, commented in mongo connection

--- server.rb
+++ server.rb
@@ -1,9 +1,38 @@
 require 'sinatra'
 
+set :public_folder, File.dirname(__FILE__) + '/public'
+set :views, File.dirname(__FILE__) + '/views'
+set :session_secret, 'supea1213sdar secadfgwr4e12312t'
+set :erb, :layout => :layout
+
+require 'mongo'
+db = nil
+shortcuts = nil
+
+# when we get a mongo server, this is how the db stuff will look, maybe
+# ENV ||= {}
+# ENV['MONGOLAB_URI'] ||= "mongodb://heroku_app1970427:v0239d4ar55rle2857h8m0ba6o@ds029217.mongolab.com:29217/heroku_app1970427"
+# uri  = URI.parse(ENV['MONGOLAB_URI'])
+# begin
+#   conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+#   if conn
+#     db = conn.db(uri.path.gsub(/^\//, ''))
+#     shortcuts = db["shortcuts"]
+#   end
+# rescue
+#   
+# end
+
 get '/' do
   "hello world"
 end
 
+# takes a single param, url and returns a json object with the shortened url
+#
+# doc: {
+#   url: "shortener.com/asb123"
+# }
 get '/shorten' do
+  redirect("/") if params["url"].nil?
   
 end
--- spec/shorten_spec.rb
+++ spec/shorten_spec.rb
@@ -21,32 +21,42 @@ describe "basic functionality" do
 end
 
 describe "URL shortener" do
+  include Rack::Test::Methods
+	
+	def app
+		Sinatra::Application
+	end
+	
+	it "should redirect to home if no url given" do
+		get '/shorten?'
+		last_response.status.should == 302
+		last_response.headers["location"].should == "http://example.org/"
+	end
 	
 	it "should return a json object containing a url" do
 		get '/shorten?url=www.google.com'
 		parsed_body = JSON.parse last_response.body
 		parsed_body["url"].nil?.should == false
-	end
- 
-	it "should redirect to home if no url given" do
-		get '/shorten?'
-		last_response.status.should == 301
-		last_response.headers["location"].should == "http://localhost:4567/"
-	end
+	end	
 end
 
 # TODO : come up with better URLs to be testing on
 describe "URL redirector" do
-
+  include Rack::Test::Methods
+	
+	def app
+		Sinatra::Application
+	end
+	
 	it "should redirect to home if no such shortened url exists" do
 		get '/NOTEXISTANT'
-		last_response.status.should == 301
-		last_response.headers["location"].should == "http://localhost:4567/"
+		last_response.status.should == 302
+		last_response.headers["location"].should == "http://example.org/"
 	end
 
 	it "should redirect me to google.com" do
 		get '/EXISTANT'
-		last.response.status.should == 200
-		last_respnse.headers["location"].should == "http://www.google.com" # using google bad?
+		last_response.status.should == 200
+		last_respnse.headers["location"].should == "http://www.google.com" # using google bad? Answer: not bad.
 	end
 end
