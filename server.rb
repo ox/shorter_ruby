ec6b034154497320df63bb11917e6dc977b09b37
Artem Titoulenko <artem.titoulenko@gmail.com>
2012-03-09 19:30:27 -0500

    removed the mongo cruft. Let's do Redis instead

--- server.rb
+++ server.rb
@@ -5,23 +5,7 @@ set :views, File.dirname(__FILE__) + '/views'
 set :session_secret, 'supea1213sdar secadfgwr4e12312t'
 set :erb, :layout => :layout
 
-require 'mongo'
-db = nil
 shortcuts = {'abc123' => "http://www.google.com/"}
-
-# when we get a mongo server, this is how the db stuff will look, maybe
-# ENV ||= {}
-# ENV['MONGOLAB_URI'] ||= "mongodb://heroku_app1970427:v0239d4ar55rle2857h8m0ba6o@ds029217.mongolab.com:29217/heroku_app1970427"
-# uri  = URI.parse(ENV['MONGOLAB_URI'])
-# begin
-#   conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
-#   if conn
-#     db = conn.db(uri.path.gsub(/^\//, ''))
-#     shortcuts = db["shortcuts"]
-#   end
-# rescue
-#   
-# end
 
 get '/' do
   "hello world"
